e73435b3fbc0baddb48bd044c3d6a5bb71496d1b
li <liningwei@liningweideMacBook-Pro.local>
2017-09-28 12:33:54 +0800

    修复bug

--- js/public.js
+++ js/public.js
@@ -3,48 +3,62 @@ $(function () {
     line()
 
     // 开关切换
+    var flag = true, time = 3, timer = null;
     $('.on-off button').on('click', function () {
-        var $this=$(this);
-        if($('.sel-button').css('display')=='none'){
+        var $this = $(this);
+        if (flag) {
+            $this.attr('disabled', true);
+            flag = false;
+        }else {
+            return false
+        }
+        setTimeout(function () {
+            $this.attr('disabled', false);
+            flag = true;
+        }, 1000)
+        if ($('.sel-button').css('display') == 'none') {
             $this.find('.icon').toggleClass('active');
             setTimeout(function () {
                 $('.sel-button').toggle()
-            },300)
+            }, 300)
             $('.mask').fadeToggle()
             $('.sel-button ul li').find('button').addClass('animate_top-active').removeClass('animate_b-active')
             $('.sel-button ul li').each(function (index) {
-                var _this=$(this);
-                anim_del(_this,index)
+                var _this = $(this);
+                anim_del(_this, index)
             })
-        }else{
+        } else {
             $('.sel-button ul li').find('button').removeClass('animate_top-active').addClass('animate_b-active')
             $($('.sel-button ul li').toArray().reverse()).each(function (index) {
-                var _this=$(this);
-                anim_del(_this,index);
+                var _this = $(this);
+                anim_del(_this, index);
             })
             setTimeout(function () {
                 $this.find('.icon').toggleClass('active');
                 $('.mask').fadeToggle()
                 $('.sel-button').toggle()
-            },450)
+            }, 450)
         }
+
     })
     $('.sel-button ul li button').each(function () {
-        $(this).on('click',function () {
+        $(this).on('click', function () {
             $('.on-off .icon').click()
-            var val=$(this).text().toString();
+            var val = $(this).text().toString();
             setTimeout(function () {
                 $('.line-title').text(val);
                 line()
-            },500)
+            }, 500)
         })
     })
-    $('.mask').on('click',function () {
+    $('.mask').on('click', function () {
         $('.on-off .icon').click()
     })
-    function anim_del(ele,index) {
+
+    function anim_del(ele, index) {
         ele.find('button').css({'animation-delay': (index / 20) + 's'});
     }
+
     function line() {
         var chart = new Highcharts.Chart('line', {
             title: {
